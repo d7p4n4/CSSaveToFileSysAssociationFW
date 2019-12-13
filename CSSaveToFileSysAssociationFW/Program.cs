a913844c337bd8d08d58904ba4d5cbc5a918b8a5
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-13 10:35:48 +0100

    Add project files.

---
+++ CSSaveToFileSysAssociationFW/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSSaveToFileSysAssociationFW
+{
+    class Program
+    {
+        private static readonly log4net.ILog _naplo = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public static string conn = ConfigurationManager.AppSettings["connString"];
+        public static SqlConnection sqlConn = new SqlConnection(ConfigurationManager.AppSettings["connString"]);
+        public static SqlConnection sqlConnXML = new SqlConnection(ConfigurationManager.AppSettings["connStringXML"]);
+        public static string TemplateName = ConfigurationManager.AppSettings["TemplateName"];
+        public static string outPath = ConfigurationManager.AppSettings["Path"];
+        public static string outPathProcess = ConfigurationManager.AppSettings["PathProcess"];
+        public static string outPathSuccess = ConfigurationManager.AppSettings["PathSuccess"];
+        public static string outPathError = ConfigurationManager.AppSettings["PathError"];
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                SaveToFileSysAssociationFW saveToFileSysAssociationFW = new SaveToFileSysAssociationFW(conn, TemplateName, outPath, outPathProcess, outPathSuccess, outPathError);
+
+                saveToFileSysAssociationFW.WriteOutAc4yObjectHome();
+                /*
+                List<SerializationObject> xmls = getXmls.GetXmlsMethod(sqlConn, sqlConnXML, TemplateName);
+                foreach(var xml in xmls)
+                {
+                    writeOut(xml.xml, xml.fileName, outPath);
+                }
+                */
+            }
+            catch (Exception exception)
+            {
+                _naplo.Error(exception.StackTrace);
+            }
+
+        }
+
+    }
+}
---
+++ CSSaveToFileSysAssociationFW/SaveToFileSysAssociationFW.cs
@@ -0,0 +1,147 @@
+﻿using CSAc4yObjectDBCap;
+using CSAc4yObjectObjectService.Association;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSSaveToFileSysAssociationFW
+{
+    class SaveToFileSysAssociationFW
+    {
+
+        public SqlConnection sqlConn;
+        public string sqlConnectionString;
+        public string TemplateName;
+        public string outPath;
+        public string outPathProcess;
+        public string outPathSuccess;
+        public string outPathError;
+
+        public SaveToFileSysAssociationFW() { }
+
+        public SaveToFileSysAssociationFW(string newSqlConn, string newTemp, string newOut, string newProc, string newSucc, string newErr)
+        {
+            sqlConnectionString = newSqlConn;
+            TemplateName = newTemp;
+            outPath = newOut;
+            outPathProcess = newProc;
+            outPathSuccess = newSucc;
+            outPathError = newErr;
+
+            sqlConn = new SqlConnection(sqlConnectionString);
+            sqlConn.Open();
+        }
+
+        public void WriteOutAc4yObjectHome()
+        {
+            Ac4yAssociationObjectService.ListInstanceResponse listInstanceResponse =
+                new Ac4yAssociationObjectService(sqlConn).ListInstance(
+                    new Ac4yAssociationObjectService.ListInstanceRequest() { }
+                );
+
+            foreach (var element in listInstanceResponse.Ac4yAssociationList)
+            {
+                string xml = serialize(element, typeof(Ac4yAssociation));
+
+                writeOut(xml, element.OriginGUID + "@" + element.OriginTemplateGUID + "@Ac4yAssociation", outPath);
+            }
+
+        }
+
+
+        public static void writeOut(string text, string fileName, string outputPath)
+        {
+            File.WriteAllText(outputPath + fileName + ".xml", text);
+
+        }
+
+        public string serialize(Object taroltEljaras, Type anyType)
+        {
+            XmlSerializer serializer = new XmlSerializer(anyType);
+            var xml = "";
+
+            using (var writer = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(writer))
+                {
+                    serializer.Serialize(writer, taroltEljaras);
+                    xml = writer.ToString(); // Your XML
+                }
+            }
+            //System.IO.File.WriteAllText(path, xml);
+
+            return xml;
+        }
+        /*
+        public void Load()
+        {
+            Ac4yObjectObjectService ac4YObjectObjectService = new Ac4yObjectObjectService(sqlConn);
+
+            try
+            {
+                string[] files =
+                    Directory.GetFiles(outPath, "*.xml", SearchOption.TopDirectoryOnly);
+
+                Console.WriteLine(files.Length);
+
+                foreach (var _file in files)
+                {
+                    string _filename = Path.GetFileNameWithoutExtension(_file);
+                    Console.WriteLine(_filename);
+                    System.IO.File.Move(outPath + _filename + ".xml", outPathProcess + _filename + ".xml");
+
+
+                    string xml = readIn(_filename, outPathProcess);
+
+                    Ac4yObjectHome tabla = (Ac4yObjectHome)deser(xml, typeof(Ac4yObjectHome));
+
+                    SetByNamesResponse response = ac4YObjectObjectService.SetByNames(
+                        new SetByNamesRequest() { TemplateName = TemplateName, Name = tabla.SimpledHumanId }
+                        );
+
+                    if (response.Result.Code.Equals("1"))
+                    {
+                        System.IO.File.Move(outPathProcess + _filename + ".xml", outPathSuccess + _filename + ".xml");
+
+                    }
+                    else
+                    {
+                        System.IO.File.Move(outPathProcess + _filename + ".xml", outPathError + _filename + ".xml");
+
+                    }
+                }
+            }
+            catch (Exception _exception)
+            {
+                Console.WriteLine(_exception.Message);
+            }
+        }
+        */
+        public Object deser(string xml, Type anyType)
+        {
+            Object result = null;
+
+            XmlSerializer serializer = new XmlSerializer(anyType);
+            using (TextReader reader = new StringReader(xml))
+            {
+                result = serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+
+        public string readIn(string fileName, string path)
+        {
+
+            string textFile = path + fileName + ".xml";
+
+            string text = File.ReadAllText(textFile);
+
+            return text;
+
+
+        }
+    }
+}
